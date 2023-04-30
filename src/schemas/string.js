fbb21559a3c0a230a25a4d7bc8e34883aab66b75
petr.gavrilov <p.gavrilov@skyeng.ru>
2023-04-30 07:34:00 +0400

    Add chaining and revise require logic

--- src/schemas/string.js
+++ src/schemas/string.js
@@ -7,33 +7,41 @@ export default class StringSchema {
 
   contains(string) {
     this.options.containStrings.push(string);
+    return this;
   }
 
   required() {
     this.options.isRequired = true;
+    return this;
   }
 
   minLength(minLength) {
     this.options.minLength = minLength;
+    return this;
   }
 
   isValid(value) {
     const checks = [];
 
     if (this.options.isRequired) {
-      checks.push(!!value);
+      checks.push((value) => !!value);
     }
 
     if (this.options.minLength) {
-      checks.push(value.length >= this.options.minLength);
+      checks.push((value) => {
+        if (!value && !this.options.required) {
+          return true;
+        }
+        return value?.length >= this.options.minLength;
+      });
     }
 
     if (this.options.containStrings.length > 0) {
-      checks.push(
+      checks.push((value) =>
         this.options.containStrings.every((string) => value.includes(string))
       );
     }
 
-    return checks.every((check) => check === true);
+    return checks.every((check) => check(value) === true);
   }
 }
--- src/tests/validator.test.js
+++ src/tests/validator.test.js
@@ -33,4 +33,20 @@ describe("validator", () => {
     schema.contains("whatthe");
     expect(schema.isValid("what does the fox say")).toBe(false);
   });
+
+  test("should validate strings with chaining", () => {
+    const v = new Validator();
+    const schema = v.string().required().minLength(4).contains("kek");
+
+    expect(schema.isValid("123kek123")).toBe(true);
+    expect(schema.isValid("kek")).toBe(false);
+  });
+
+  test("should validate not required string with minLength", () => {
+    const v = new Validator();
+    const schema = v.string().minLength(4);
+
+    expect(schema.isValid(undefined)).toBe(true);
+    expect(schema.isValid("vas")).toBe(false);
+  });
 });
